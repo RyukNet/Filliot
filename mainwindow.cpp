857c8ac6171d639e4f1d76febd81dd4895908e16
RyukNet <38398145+RyukNet@users.noreply.github.com>
2020-12-12 03:53:33 +0200

    first commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,8 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    fileSystem.setRootPath(QDir::currentPath());
+    ui->treeWidget->QTreeView::setModel(&fileSystem);
 }
 
 MainWindow::~MainWindow()
